32f630db8978f531b8e7e0596420ee6d4d1b0513
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-06-10 18:49:25 +0530

    Added script for first introduction

---
+++ greeter.ts
@@ -0,0 +1,22 @@
+// public on arguments to the constructor is a shorthand that allows us
+// to automatically create properties with that name.
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = `${firstName} ${middleInitial} ${lastName}`
+    }
+}
+
+interface Person {
+    firstName: string,
+    lastName: string
+}
+
+function greeter(person: Person) {
+    return `Hello, ${person.firstName} ${person.lastName}`
+}
+
+let user = new Student("Jane", "M.", "User")
+
+console.log(greeter(user))
